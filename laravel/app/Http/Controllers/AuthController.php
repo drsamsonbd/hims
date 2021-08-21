ea735abdc6bfcbb56f11bb1711902425b7fcd6c7
drsamsonbd <drsamsonbd@gmail.com>
2021-08-21 14:25:20 +0800

    JWT MIDDLEWARE

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -16,7 +16,8 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+       $this->middleware('JWT', ['except' => ['login']]);
+      //   $this->middleware('auth:api', ['except' => ['login']]);
     }
   
     /**
