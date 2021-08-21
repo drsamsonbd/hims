3ee25eb9e3b8ab70c1e86b2ef0f479e9046f1884
drsamsonbd <drsamsonbd@gmail.com>
2021-08-21 11:02:31 +0800

    Update api.php
    menu to cors

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -15,12 +15,15 @@ use Illuminate\Http\Request;
 
 Route::middleware('cors')->group(function(){
     Route::post('login', 'AuthController@login');
+    Route::get('menu', 'MenuController@index');
+
+
+
  });
 
 
 Route::group(['middleware' => 'api'], function ($router) {
-    Route::get('menu', 'MenuController@index');
-
+   
  
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
