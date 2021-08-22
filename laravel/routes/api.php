df39d015846b5a1105679d15ee52273e0980bafc
drsamsonbd <drsamsonbd@gmail.com>
2021-08-22 10:52:39 +0800

    Update api.php

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-header('Access-Control-Allow-Origin: *,*');
+header('Access-Control-Allow-Origin:  https://his.hospbeluran.fastq.co');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
 
