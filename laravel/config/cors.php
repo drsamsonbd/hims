a0f0e130b9b89cd65b9decca30b1ec166d75b636
drsamsonbd <drsamsonbd@gmail.com>
2021-08-24 00:14:25 +0800

    Update cors.php

--- laravel/config/cors.php
+++ laravel/config/cors.php
@@ -31,17 +31,17 @@ return [
     /*
      * Matches the request origin. `[*]` allows all origins. Wildcards can be used, eg `*.mydomain.com`
      */
-   // 'allowed_origins' => ['*'],
+    'allowed_origins' => ['*'],
 
     /*
      * Patterns that can be used with `preg_match` to match the origin.
      */
-//    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [],
 
     /*
      * Sets the Access-Control-Allow-Headers response header. `[*]` allows all headers.
      */
-  //  'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'],
 
     /*
      * Sets the Access-Control-Expose-Headers response header with these headers.
