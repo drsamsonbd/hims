fac87b5241374f354d2e829ac5829b5c62ba6b18
drsamsonbd <drsamsonbd@gmail.com>
2021-08-24 08:39:47 +0800

    api address

--- laravel/config/index.js
+++ laravel/config/index.js
@@ -1,8 +1,7 @@
-
 module.exports = {
 proxyTable: {
     '/api': {
-      target: 'https://his.hospbeluran.fastq.co',
+      target: 'http://127.0.0.1:8000',
       changeOrigin: true,
       pathRewrite: {
         '^/api': ''
