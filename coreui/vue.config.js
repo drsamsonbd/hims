20df8db1709fd683822453828c673f469436b8d2
drsamsonbd <drsamsonbd@gmail.com>
2021-08-24 08:50:10 +0800

    remove dev

--- coreui/vue.config.js
+++ coreui/vue.config.js
@@ -6,13 +6,5 @@ module.exports = {
     resolve: {
        symlinks: false
     }
-  },
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true
-      }
-    }
   }
 }
